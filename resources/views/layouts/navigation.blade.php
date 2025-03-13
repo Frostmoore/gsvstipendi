ac7c9e1146b4b58d3d97a495cb61dad21ac12367
Frostmoore <ronconi.ric@gmail.com>
2025-03-13 01:37:23 +0100

    cazzate

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -28,11 +28,6 @@
                 @auth
                     @if(Auth::user()->role == 'admin' || Auth::user()->role == 'superadmin')
                         <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                            <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                                {{ __('Fogli Orari') }}
-                            </x-nav-link>
-                        </div>
-                        <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
                             <x-nav-link :href="route('utenti.index')" :active="request()->routeIs('utenti.index')">
                                 {{ __('Utenti') }}
                             </x-nav-link>
